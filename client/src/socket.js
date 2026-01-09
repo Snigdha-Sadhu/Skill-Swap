0d68695f4c7d57e6284b4844b8fcdbeba991e1fd
Snigdha-Sadhu <snigdhasadhu2005@gmail.com>
2026-01-09 08:18:41 +0530

    updated

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,5 +1,5 @@
 import {io} from "socket.io-client";
-const socket=io("http://localhost:7000",{
+const socket=io("https://skill-swap-backend-7a5g.onrender.com",{
     withCredentials:true,
     autoConnect:true,
     /*extraHeaders: {
